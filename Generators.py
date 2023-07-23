453031f533015f30f46aaccca9c213ce1c1e8541
Melikşah Dipçin <52791785+meliksahdipcin@users.noreply.github.com>
2023-07-23 23:34:30 +0300

    Add files via upload

---
+++ Generators.py
@@ -0,0 +1,20 @@
+def create_cubes(n):
+    for x in range(n):
+        yield x**3
+    
+for x in create_cubes(10):
+    print(x)
+    
+    
+
+
+def gen_fibon(n):
+    a = 1
+    b = 1
+    
+    for i in range(n):
+        yield a
+        a,b = b,b+a
+
+for number in gen_fibon(10):
+    print(number)
